dabbc218529648329db5a0a8e49deadcf20adeb8
Manuel <41003840s@iespoblenou.org>
2016-03-10 18:40:02 +0100

    2

--- factory.js
+++ factory.js
@@ -1,1 +1,38 @@
 // this factory returns a synchronized array of chat messages
+app.factory("getUser", ["$firebaseObject",
+    function($firebaseObject) {
+        return function(usuari) {
+            var ref = new Firebase("https://ecaibtweet.firebaseio.com/users");
+
+            return {nom: $firebaseObject(ref.child(usuari).child("name")),
+                desc: $firebaseObject(ref.child(usuari).child("description"))};
+        };
+    }
+]);
+
+app.factory("getUserTweets", ["$firebaseArray",
+    function($firebaseArray) {
+        return function(usuari) {
+            var ref = new Firebase("https://ecaibtweet.firebaseio.com/users");
+            return $firebaseArray(ref.child(usuari).child("tweets"));
+        };
+    }
+]);
+
+app.factory("getFollowings", ["$firebaseArray",
+    function($firebaseArray) {
+        return function(usuari) {
+            var ref = new Firebase("https://ecaibtweet.firebaseio.com/users");
+            return $firebaseArray(ref.child(usuari).child("following"));
+        };
+    }
+]);
+
+app.factory("getFollowingTweets", ["$firebaseArray",
+    function($firebaseArray) {
+        return function(usuari) {
+            var ref = new Firebase("https://ecaibtweet.firebaseio.com/users");
+            return $firebaseArray(ref.child(usuari).child("following"));
+        };
+    }
+]);
